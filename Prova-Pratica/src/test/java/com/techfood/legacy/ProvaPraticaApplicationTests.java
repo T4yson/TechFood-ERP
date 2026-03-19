149770d72f9f082e03d1518b56a1cbac6f5dac96
Igor Tayson Bresolin Savero <igor_savero@estudante.sesisenai.org.br>
2026-03-19 15:36:38 -0300

    feat: corrige e inicia prova

---
+++ Prova-Pratica/src/test/java/com/techfood/legacy/ProvaPraticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techfood.legacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaPraticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
